456badfba851fd243115e590a520bf483892e961
darylldavidfst <daryll.david@fullspeedtechnologies.com>
2022-09-12 17:36:25 +0800

    first commit

---
+++ src/main/java/com/fst/bazaar/FstbazaarApplication.java
@@ -0,0 +1,13 @@
+package com.fst.bazaar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FstbazaarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FstbazaarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fst/bazaar/FstbazaarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fst.bazaar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FstbazaarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
