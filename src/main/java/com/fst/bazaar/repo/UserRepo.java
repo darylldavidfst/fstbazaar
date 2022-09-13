531718279f5658e29c2413519a8941e6f2f2a279
darylldavidfst <daryll.david@fullspeedtechnologies.com>
2022-09-13 13:30:52 +0800

    added @Repository annotation

--- src/main/java/com/fst/bazaar/repo/UserRepo.java
+++ src/main/java/com/fst/bazaar/repo/UserRepo.java
@@ -1,9 +1,11 @@
 package com.fst.bazaar.repo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.fst.bazaar.entity.User;
 
+@Repository
 public interface UserRepo extends JpaRepository<User, Integer> {
 	User findByEmail(String email);
 }
