f7a004f006e23166c84876877479489536963317
darylldavidfst <daryll.david@fullspeedtechnologies.com>
2022-09-13 13:35:27 +0800

    added @NoArgsConstructor

--- src/main/java/com/fst/bazaar/entity/Role.java
+++ src/main/java/com/fst/bazaar/entity/Role.java
@@ -8,10 +8,12 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "role")
 @Data
+@NoArgsConstructor
 public class Role {
 
 	@Id
