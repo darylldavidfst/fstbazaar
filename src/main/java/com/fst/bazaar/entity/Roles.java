1cd464d9dd30656ddab806b024550f86a2d7be77
darylldavidfst <daryll.david@fullspeedtechnologies.com>
2022-09-13 15:22:53 +0800

    Auto-Formatted Roles enum

--- src/main/java/com/fst/bazaar/entity/Roles.java
+++ src/main/java/com/fst/bazaar/entity/Roles.java
@@ -1,14 +1,14 @@
 package com.fst.bazaar.entity;
 
 public enum Roles {
-    Admin("Manage everything"),
-    Salesperson("Manage product price, customers, shipping, orders and sales report"),
-    Editor("Manage categories, brands, products, articles and menus"),
-    Shipper("View products, view orders and update order status");
+	Admin("Manage everything"),
+	Salesperson("Manage product price, customers, shipping, orders and sales report"),
+	Editor("Manage categories, brands, products, articles and menus"),
+	Shipper("View products, view orders and update order status");
 
-    public final String DESCRIPTION;
+	public final String DESCRIPTION;
 
-    Roles(String description) {
-        this.DESCRIPTION = description;
-    }
+	Roles(String description) {
+		this.DESCRIPTION = description;
+	}
 }
