bdde6a96665b3318e67d203aae2365dd500b65c4
darylldavidfst <daryll.david@fullspeedtechnologies.com>
2022-09-13 13:17:32 +0800

    removed spaces

--- src/main/java/com/fst/bazaar/repo/RoleRepo.java
+++ src/main/java/com/fst/bazaar/repo/RoleRepo.java
@@ -5,6 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.fst.bazaar.entity.Role;
 
 public interface RoleRepo extends JpaRepository<Role, Integer> {
-
 	Role findByName(String name);
 }
--- src/main/java/com/fst/bazaar/repo/UserRepo.java
+++ src/main/java/com/fst/bazaar/repo/UserRepo.java
@@ -5,6 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.fst.bazaar.entity.User;
 
 public interface UserRepo extends JpaRepository<User, Integer> {
-
 	User findByEmail(String email);
 }
