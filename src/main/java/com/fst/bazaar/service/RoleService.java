22a4683b97624d5ce49f0dc9e161ee8713a74fc4
darylldavidfst <daryll.david@fullspeedtechnologies.com>
2022-09-13 13:17:39 +0800

    removed spaces

--- src/main/java/com/fst/bazaar/service/RoleService.java
+++ src/main/java/com/fst/bazaar/service/RoleService.java
@@ -6,16 +6,10 @@ import java.util.List;
 import com.fst.bazaar.entity.Role;
 
 public interface RoleService {
-
 	List<Role> findAll();
-
 	Role findOne(int id);
-
 	Role findByName(String name);
-
 	void deleteAll();
-
 	void fillRoles();
-
 	ArrayList<Integer> getRolesIds();
 }
