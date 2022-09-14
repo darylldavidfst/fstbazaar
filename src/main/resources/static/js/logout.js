3b9be733ac1dbbc1f9aa51ba01535a19ea4cb213
CloydAnis <cloyd.anis@fullspeedtechnologies.com>
2022-09-14 15:06:38 +0800

    Apply autoformat and removed whitespaces

--- src/main/resources/static/js/logout.js
+++ src/main/resources/static/js/logout.js
@@ -3,9 +3,9 @@ $(document).ready(function () {
         e.preventDefault();
         document.logoutForm.submit();
     });
-
     customizeDropDownMenu();
 })
+
 function customizeDropDownMenu() {
     $(".dropdown > a").click(function () {
         location.href = this.href;
