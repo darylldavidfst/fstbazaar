e0c4ddfc095b19e7e67289997b5d720a16f84a46
darylldavidfst <daryll.david@fullspeedtechnologies.com>
2022-09-15 09:24:18 +0800

    commit

--- src/main/java/com/fst/bazaar/service/UserDetailsImplementation.java
+++ src/main/java/com/fst/bazaar/service/UserDetailsImplementation.java
@@ -29,7 +29,7 @@ public class UserDetailsImplementation implements UserDetailsService {
 			throw new UsernameNotFoundException(email);
 		}
 
-		System.out.println("here");
+		System.out.println(user);
 
 		List<SimpleGrantedAuthority> authorities = user.getRoles()
 				.stream()
